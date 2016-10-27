668f61d523cd931bcb74acf67cdba6256c728e28
Nick Babcock <nbabcock19@hotmail.com>
2016-10-26 20:15:55 -0400

    Allow user to override configured JsonProvider

--- dropwizard-e2e/src/main/java/com/example/app1/App1.java
+++ dropwizard-e2e/src/main/java/com/example/app1/App1.java
@@ -1,11 +1,5 @@
 package com.example.app1;
 
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-
-import com.github.mustachejava.MustacheNotFoundException;
-
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.jersey.optional.EmptyOptionalException;
@@ -13,13 +7,19 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
 import io.dropwizard.views.ViewRenderException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.glassfish.jersey.spi.ExtendedExceptionMapper;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 
 public class App1 extends Application<Configuration> {
     @Override
     public void initialize(Bootstrap<Configuration> bootstrap) {
-        bootstrap.addBundle(new ViewBundle<Configuration>());
+        bootstrap.addBundle(new ViewBundle<>());
     }
-    
+
     @Override
     public void run(Configuration config, Environment env) throws Exception {
         // Ensure that we can override the default 404 response on an
@@ -30,20 +30,23 @@ public class App1 extends Application<Configuration> {
                 return Response.noContent().build();
             }
         });
-        
+
         // Ensure that we can override the 503 response of a view that refers to
         // a missing Mustache template and return a 404 instead
-        env.jersey().register(new ExceptionMapper<WebApplicationException>() {
+        env.jersey().register(new ExtendedExceptionMapper<WebApplicationException>() {
             @Override
             public Response toResponse(WebApplicationException exception) {
-                if (exception.getCause() instanceof ViewRenderException
-                        && exception.getCause().getCause() instanceof MustacheNotFoundException) {
-                    return Response.status(Response.Status.NOT_FOUND).build();
-                }
-                return exception.getResponse();
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+
+            @Override
+            public boolean isMappable(WebApplicationException e) {
+                return ExceptionUtils.indexOfThrowable(e, ViewRenderException.class) != -1;
             }
         });
 
         env.jersey().register(new App1Resource());
+        env.jersey().register(new CustomJsonProvider(env.getObjectMapper()));
+        env.jersey().register(new CustomClassBodyWriter());
     }
 }
